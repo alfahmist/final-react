743d035a51ee05c33aefc8e4b2c706e943e302c0
Fahmi <gst.alfahmi@gmail.com>
2021-07-27 11:39:54 +0700

    modul login

--- src/molecules/Login.js
+++ src/molecules/Login.js
@@ -1,21 +1,25 @@
-import React from 'react'
-import {Form} from 'react-bootstrap'
-function Login(props) {
-    return (
-        <div>
-            <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                <Form.Label>Username</Form.Label>
-                <Form.Control type="email" placeholder="Masukkan username" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Masukkan Password" />
-            </Form.Group>
-            </Form>
-        </div>  
-    )
+import React, {Component} from 'react'
+import {Form, Button} from 'react-bootstrap'
+export default class Login extends Component {
+    render(){
+        let {handleChange, handleSubmit} = this.props;
+        return (
+         
+            <div>
+                <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control name="username" type="text" placeholder="Masukkan username" onChange={handleChange} />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control name="password" type="password" placeholder="Masukkan Password" onChange={handleChange} />
+                </Form.Group>
+                <Button variant="primary" onClick={handleSubmit}>
+                    Masuk
+                </Button>
+                </Form>
+            </div>  
+        )
+    }
 }
-
-export default Login
-
--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,8 +1,72 @@
-import React from 'react'
-import { Navbar, Nav, Container, Button } from 'react-bootstrap'
+import React, { Component } from 'react'
+import { Navbar, Nav, Container, Button, Modal } from 'react-bootstrap'
 import logo from '../logo.svg'
-function Header() {
-    return (
+import LoginForm from '../molecules/Login'
+
+
+export default class Header extends Component {
+    constructor(props) {
+        super(props)
+        this.handleModal = this.handleModal.bind(this);
+        this.handleClose = this.handleClose.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+        this.state = {
+            show : false,
+            isLoggedIn : false,
+            username: "",
+            password: ""
+        }
+    }
+    
+    handleModal(){
+        this.setState({
+            show: true
+        })
+    }
+    
+    handleClose() {
+        this.setState({
+            show: false
+        })
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name] : event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        let user = {
+            username : "admin",
+            password : "admin"
+        }
+        let {username, password} = this.state
+        if (user.username === username && user.password === password)
+        {
+            this.setState({
+                show: false,
+                username : "",
+                password : "",
+                isLoggedIn : true
+            })
+        }
+        else {
+            alert("username atau password salah")
+        }
+        event.stopPropagation();
+        event.preventDefault();
+    }
+
+    handleLogout() {
+        this.setState({
+            isLoggedIn : false
+        })
+    }
+    render() {
+        return (
         <div>
             <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
@@ -19,15 +83,42 @@ function Header() {
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="me-auto">
                 </Nav>
-                    <Button variant="outline-success">Masuk</Button>
-                    <div className="margin-left-2">
-                        <Button variant="success">Daftar</Button>
-                    </div>
+                {
+                    this.state.isLoggedIn ?  
+                    <Button variant="outline-success" onClick={this.handleLogout}>keluar</Button>:
+                    <Button variant="outline-success" onClick={this.handleModal}>Masuk</Button> 
+                }
+                {
+                    this.state.isLoggedIn ?  <div></div>:
+                <div className="margin-left-2">
+                     <Button variant="success">Daftar</Button>
+                 </div>
+                }
+                   
+                 
+                   
             </Navbar.Collapse>
             </Container>
             </Navbar>
-        </div>
-    )
-}
 
-export default Header
+            <Modal
+                show={this.state.show}
+                onHide={this.handleClose}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+                >
+                <Modal.Header closeButton>
+                    <Modal.Title id="contained-modal-title-vcenter" onClick={this.handleClose}>
+                    Login
+                    </Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <LoginForm {...this.state} handleChange={this.handleChange} handleSubmit={this.handleSubmit}/>
+                </Modal.Body>
+                </Modal> 
+        </div>  
+    
+        )
+    }
+}
